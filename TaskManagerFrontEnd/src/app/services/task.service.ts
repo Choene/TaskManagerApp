7e88c544e1c0a1da718f97bcc91113ade6a65a15
Choene Chokoe <choene.chokoe@cyintecs.co.za>
2025-02-26 22:00:31 +0200

    Added Environments for local dev and prod

--- TaskManagerFrontEnd/src/app/services/task.service.ts
+++ TaskManagerFrontEnd/src/app/services/task.service.ts
@@ -2,10 +2,11 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Task } from '../models/task.model';
+import { environment } from '../../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class TaskService {
-  private apiUrl = 'https://localhost:7021/api/tasks';
+  private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
---
+++ TaskManagerFrontEnd/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://taskmanagerapi-gfg7hcesd6bkh4ct.southafricanorth-01.azurewebsites.net/api/tasks'
+};
