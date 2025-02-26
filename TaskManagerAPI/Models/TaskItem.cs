0519e3aba99061bad45db93244490532cd4c4997
Choene Chokoe <choene.chokoe@cyintecs.co.za>
2025-02-26 18:26:58 +0200

    Task Id Annotation

--- TaskManagerAPI/Models/TaskItem.cs
+++ TaskManagerAPI/Models/TaskItem.cs
@@ -1,10 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace TaskManagerAPI.Models
 {
     public class TaskItem
     {
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
         [Required]
