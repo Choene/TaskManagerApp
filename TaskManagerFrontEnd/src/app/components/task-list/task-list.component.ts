4b2e1fe3c5e270022823a2351423afd566482bbc
Choene Chokoe <choene.chokoe@cyintecs.co.za>
2025-02-26 18:48:38 +0200

    Task list

--- TaskManagerFrontEnd/src/app/components/task-list/task-list.component.ts
+++ TaskManagerFrontEnd/src/app/components/task-list/task-list.component.ts
@@ -1,11 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TaskService } from '../../services/task.service';
+import { CommonModule } from '@angular/common';
+import { TaskItemComponent } from '../task-item/task-item.component';
+import { Task } from '../../models/task.model';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-task-list',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, TaskItemComponent, FormsModule],
   templateUrl: './task-list.component.html',
-  styleUrl: './task-list.component.css'
+  styleUrls: ['./task-list.component.css'],
 })
-export class TaskListComponent {
+export class TaskListComponent implements OnInit {
+  tasks: Task[] = [];
 
+  constructor(private taskService: TaskService) {}
+
+  ngOnInit(): void {
+    this.loadTasks();
+  }
+
+  loadTasks(): void {
+    this.taskService.getTasks().subscribe((tasks) => {
+      this.tasks = tasks.map((task) => ({
+        ...task,
+        isEditing: false,
+      }));
+    });
+  }
+
+  addTask(newTask: Task): void {
+    this.taskService.addTask(newTask).subscribe((task) => {
+      this.tasks = [...this.tasks, { ...task, isEditing: false }];
+    });
+  }
+
+  editTask(task: Task): void {
+    task.isEditing = true;
+  }
+
+  saveTask(task: Task): void {
+    this.taskService.updateTask(task).subscribe(() => {
+      task.isEditing = false;
+    });
+  }
+
+  cancelEdit(task: Task): void {
+    task.isEditing = false;
+    this.loadTasks();
+  }
+
+  deleteTask(id: number): void {
+    this.taskService.deleteTask(id).subscribe(() => {
+      this.tasks = this.tasks.filter((task) => task.id !== id);
+    });
+  }
 }
