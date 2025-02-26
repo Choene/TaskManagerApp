bbc82f25c70ed8d2b3aa5ed38b3683526620f5c3
Choene Chokoe <choene.chokoe@cyintecs.co.za>
2025-02-26 18:48:13 +0200

    Task form

--- TaskManagerFrontEnd/src/app/components/task-form/task-form.component.ts
+++ TaskManagerFrontEnd/src/app/components/task-form/task-form.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { TaskService } from '../../services/task.service';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Task } from '../../models/task.model';
 
 @Component({
   selector: 'app-task-form',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './task-form.component.html',
-  styleUrl: './task-form.component.css'
+  styleUrls: ['./task-form.component.css'],
 })
 export class TaskFormComponent {
+  @Output() taskAdded = new EventEmitter<Task>();
+  task: Task = { id: 0, title: '', description: '', dueDate: new Date().toISOString() };
 
+  constructor(private taskService: TaskService) {}
+
+  addTask(): void {
+    if (!this.task.title) return;
+
+    this.taskService.addTask(this.task).subscribe(() => {
+      this.taskAdded.emit(this.task);
+      this.task = { id: 0, title: '', description: '', dueDate: new Date().toISOString() };
+    });
+  }
+
+  submitTask(): void {
+    this.addTask();
+  }
 }
